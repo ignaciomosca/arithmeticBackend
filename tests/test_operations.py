b37048a34bca39047b02263cf3c3b24ba831226e
Ignacio Mosca <ignaciomosca@gmail.com>
2024-10-01 22:15:24 -0300

    Fix tests names

--- tests/test_operations.py
+++ tests/test_operations.py
@@ -19,7 +19,7 @@ async def test_addition_operation(
     authenticated_client: AsyncClient,
     dbsession: AsyncSession,
 ) -> None:
-    """Tests performing the perform an addition."""
+    """Test performing an addition."""
     url = fastapi_app.url_path_for("new_operation")
     operation = OperationBase(type=OperationEnum.ADDITION, first_term=1, second_term=2)
     dao = RecordDAO(dbsession)
@@ -30,13 +30,13 @@ async def test_addition_operation(
     await operation_dao.add_new_operation(OperationEnum.ADDITION.value, 1)
 
     await operation_dao.get_all_operations()
-    initial_count = await dao.get_all_records(user_id=1, limit=1, offset=0)
+    initial_count = await dao.get_all_records(user_id=with_user_id, limit=1, offset=0)
     # Send a POST request to create the operations
     response = await authenticated_client.post(url, json=operation.model_dump())
     # Assert the response status code is 201 OK
     assert response.status_code == status.HTTP_201_CREATED
     # Use the DAO again to count operations instances after creation
-    final_count = await dao.get_all_records(user_id=1, limit=1, offset=0)
+    final_count = await dao.get_all_records(user_id=with_user_id, limit=1, offset=0)
     user = await user_dao.get_user_by_id(with_user_id)
     # Assert that the count increased by 1 after creation
     assert final_count.total_count == initial_count.total_count + 1
@@ -50,7 +50,7 @@ async def test_substraction_operation(
     authenticated_client: AsyncClient,
     dbsession: AsyncSession,
 ) -> None:
-    """Tests performing the perform a substraction."""
+    """Test performing a substraction."""
     url = fastapi_app.url_path_for("new_operation")
     operation = OperationBase(
         type=OperationEnum.SUBTRACTION,
@@ -85,7 +85,7 @@ async def test_multiplication_operation(
     authenticated_client: AsyncClient,
     dbsession: AsyncSession,
 ) -> None:
-    """Tests performing the perform a multiplication."""
+    """Test performing a multiplication."""
     url = fastapi_app.url_path_for("new_operation")
     operation = OperationBase(
         type=OperationEnum.MULTIPLICATION,
@@ -120,7 +120,7 @@ async def test_division_operation(
     authenticated_client: AsyncClient,
     dbsession: AsyncSession,
 ) -> None:
-    """Tests performing the perform a division."""
+    """Tests performing a division."""
     url = fastapi_app.url_path_for("new_operation")
     operation = OperationBase(type=OperationEnum.DIVISION, first_term=4, second_term=2)
     dao = RecordDAO(dbsession)
@@ -151,7 +151,7 @@ async def test_division_by_zero_operation(
     authenticated_client: AsyncClient,
     dbsession: AsyncSession,
 ) -> None:
-    """Tests performing the perform a division by zero and check for error."""
+    """Test performing a division by zero and checking for an error."""
     url = fastapi_app.url_path_for("new_operation")
 
     operation_data = {"type": "DIVISION", "first_term": 4, "second_term": 0}
@@ -172,7 +172,7 @@ async def test_square_root_operation(
     authenticated_client: AsyncClient,
     dbsession: AsyncSession,
 ) -> None:
-    """Tests performing the perform a square root."""
+    """Test performing a square root."""
     url = fastapi_app.url_path_for("new_operation")
     operation = OperationBase(type=OperationEnum.SQUARE, first_term=4)
     dao = RecordDAO(dbsession)
@@ -203,7 +203,7 @@ async def test_square_root_of_negative_number_operation(
     authenticated_client: AsyncClient,
     dbsession: AsyncSession,
 ) -> None:
-    """Tests performing the square root of a negative number and check for error."""
+    """Test performing the square root of a negative number and checking an error."""
     url = fastapi_app.url_path_for("new_operation")
     operation_dao = OperationDAO(dbsession)
     await operation_dao.add_new_operation(OperationEnum.SQUARE.value, 5)
@@ -225,7 +225,7 @@ async def test_random_string(
     dbsession: AsyncSession,
     monkeypatch: pytest.MonkeyPatch,
 ) -> None:
-    """Tests performing the perform random operation function."""
+    """Test performing the random operation function."""
 
     # Create a mock for the httpx.AsyncClient.post method
     async def mock_post(*args: tuple, **kwargs: dict) -> AsyncMock:
